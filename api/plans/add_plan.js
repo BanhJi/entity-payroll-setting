cd8c09aa22ff24d7c1035665c503ca1d7e90b66c
Thearann Banhji <75470342+Thearann-Banhji@users.noreply.github.com>
2023-02-24 18:00:26 +0700

    setting payroll

---
+++ api/plans/add_plan.js
@@ -0,0 +1,67 @@
+'use strict'
+
+const AWS = require('aws-sdk')
+const code = require('../../config/code.js')
+const message = require('../../config/message.js')
+const json = require('../../config/response.js')
+const uuid = require('uuid')
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+
+module.exports.index = async (event) => {
+  const timestamp = new Date().toJSON()
+  const data = JSON.parse(event.body)
+  const table = process.env.item_table
+  const instituteId = event.pathParameters.institute_id
+  let head = 'ppl-' // payroll bank
+  if (data.id === undefined || data.id === '') {
+    head = 'ppl-' + uuid.v1()
+  } else {
+    head = data.id
+  }
+  const pk = head
+  const params = {
+    TableName: table,
+      Item: {
+        pk:         pk,
+        sk:         instituteId,
+        planCode:   data.planCode,
+        planName:   data.planName,
+        planItems:   data.planItems,
+        description: data.description,
+        createdAt:  timestamp,
+        updatedAt:  timestamp
+      }
+  };
+  //  todo: write to the database
+  try {
+    await dynamoDb
+      .put( params)
+      .promise()
+    // response back
+    const response = {
+      id:         pk,
+      planCode:  data.planCode,
+      planName:  data.planName,
+      description: data.description,
+      planItems:  data.planItems,
+    }
+
+    return {
+      statusCode: code.httpStatus.Created,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*' // to allow cross origin access
+      },
+      body: json.responseBody(code.httpStatus.Created, response, message.msg.ItemCreatedSuccessed, '', 1)
+    }
+  } catch (err) {
+    return {
+      statusCode: code.httpStatus.BadRequest,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*' // to allow cross origin access
+      },
+      body: json.responseBody(code.httpStatus.BadRequest, [], message.msg.ItemCreatedFailed, err, 0)
+    }
+  }
+}
---
+++ api/plans/get_plan.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const AWS = require('aws-sdk')
+const code = require('../../config/code.js')
+const message = require('../../config/message.js')
+const json = require('../../config/response.js')
+const uuid = require('uuid')
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+
+module.exports.get = async (event, context) => {
+  const table = process.env.item_table
+  const params = {
+    TableName: table,
+    IndexName: 'GSI1',
+    KeyConditionExpression: 'sk = :sk AND begins_with(pk, :type)',
+    ExpressionAttributeValues: {
+      ':sk': event.pathParameters.institute_id,
+      ':type': 'ppl-'
+    },
+  }
+  try {
+    const data = await dynamoDb.query(params).promise()
+    const results = data.Items.map(item => {
+      return {
+        id:         item.pk,
+        planCode:   item.planCode,
+        planName:   item.planName,
+        description: item.description,
+        planItems:   item.planItems
+      }
+    })
+    return {
+      statusCode: code.httpStatus.OK,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*' // to allow cross origin access
+      },
+      body: json.responseBody(code.httpStatus.OK, results, message.msg.FetchSuccessed, '', 1)
+    }
+  } catch (error) {
+    return {
+      statusCode: code.httpStatus.Created,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*' // to allow cross origin access
+      },
+      body: json.responseBody(code.httpStatus.BadRequest, [], message.msg.FetchFailed, error, 0)
+    }
+  }
+}
